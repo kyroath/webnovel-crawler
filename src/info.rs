ddd1b1aa964b47e8973c6e63c570ceec60217e29
Fatih Karahan <hseyin97@gmail.com>
2020-07-08 00:29:41 +0300

    added a simple test and travis integration

--- src/info.rs
+++ src/info.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum Source {
     Fanfiction,
     NotGiven,
@@ -9,7 +10,7 @@ impl std::default::Default for Source {
     }
 }
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct FictionInfo {
     pub host: Source,
     pub id: String,
@@ -19,9 +20,33 @@ pub struct FictionInfo {
 }
 
 impl FictionInfo {
+    pub fn parse_url_str(url: &str) -> Self {
+        FictionInfo::parse_url(url.to_string())
+    }
+
     pub fn parse_url(url: String) -> Self {
         FictionInfo {
             ..Default::default()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_info() {
+        let t = FictionInfo {
+            ..Default::default()
+        };
+
+        let parsed = FictionInfo::parse_url_str("test");
+
+        assert_eq!(parsed.host, t.host);
+        assert_eq!(parsed.id, t.id);
+        assert_eq!(parsed.name, t.name);
+        assert_eq!(parsed.chapterCount, t.chapterCount);
+        assert_eq!(parsed.wordCount, t.wordCount);
     }
 }
